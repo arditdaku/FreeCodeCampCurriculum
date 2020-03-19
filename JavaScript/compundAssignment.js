8f2dd151ec1624f5f0c76b0cf4abfba0cd25d71a
MagicalWaffle <ardit.daku@yahoo.com>
2020-03-19 13:52:08 +0100

    basic js compound

---
+++ JavaScript/compundAssignment.js
@@ -0,0 +1,45 @@
+// In programming, it is common to use assignments to modify the contents of a variable. Remember that everything to the right of the equals sign is evaluated first, so we can say:
+
+// myVar = myVar + 5;
+
+// to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
+
+// One such operator is the += operator.
+
+
+var a = 3;
+var  b= 17;
+var c= 12;
+
+
+a+=12;
+b+=9;
+c+=7;
+
+//same goes to subtraction
+
+var x = 11;
+var y= 19;
+var z= 12;
+
+x-=6;
+y-=9;
+z-=2;
+
+/// and MultiplicationPassed
+var x1 = 11;
+var y1= 19;
+var z1= 12;
+
+x1*=2;
+y1*=3;
+z1*=4;
+//Division 
+
+var x2 = 11;
+var y2= 19;
+var z2= 12;
+
+x2/=3;
+y2/=9;
+z2/=3;
---
+++ JavaScript/decimalNumber.js
@@ -0,0 +1,18 @@
+// decimal number in js
+
+var ourDecimal = 5.7;
+
+// create a decimal number with variable name myDecimal
+
+var myDecimal= 99.99;
+
+
+var deicmalnumber1=  2.5 * 2.2;
+
+
+var decmailnumber2= 4.4/2;
+
+
+
+var remainder = 11% 3;
+
